eb747f1238d12429bdc4d8d15b0a71f090a06729
Sylvester-Ad <sylvesteradade895@gmail.com>
2025-04-17 01:59:44 +0000

    Change color of icons to grey in dark-theme

--- docs/js/script.js
+++ docs/js/script.js
@@ -83,7 +83,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     window.onload = fadeOut;
 
-    document.getElementById('theme-toggle').addEventListener('click', () => {
+    const themeToggle = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme === 'dark') {
+        document.body.classList.add('dark-theme');
+    }
+
+    themeToggle.addEventListener('click', () => {
         document.body.classList.toggle('dark-theme');
+        const theme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+        localStorage.setItem('theme', theme);
     });
 });
