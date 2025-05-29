3b2a148643d262a90fd4c4078b8b10db3dd67aa8
Sylvester-Ad <sylvesteradade895@gmail.com>
2025-05-29 19:08:10 +0000

    Add routes for updating and deleting leads

--- cs50w/python/django_projects/djcrm/leads/urls.py
+++ cs50w/python/django_projects/djcrm/leads/urls.py
@@ -5,5 +5,7 @@ app_name = "leads"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:pk>/", views.lead_detail, name="lead_detail"),
+    path("<int:pk>/update/", views.lead_update, name="lead_update"),
+    path("<int:pk>/delete/", views.lead_delete, name="lead_delete"),
     path("create/", views.lead_create, name="lead_create"),
 ]
