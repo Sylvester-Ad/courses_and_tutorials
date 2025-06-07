39a3544703b6316b52d36feee8df5dadbbdf11f6
Sylvester-Ad <sylvesteradade895@gmail.com>
2025-06-07 00:33:03 +0000

    Register UserProfile Model

--- cs50w/python/django_projects/djcrm/leads/admin.py
+++ cs50w/python/django_projects/djcrm/leads/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Lead, Agent
+from .models import User, Lead, Agent, UserProfile
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ from .models import User, Lead, Agent
 admin.site.register(User)
 admin.site.register(Lead)
 admin.site.register(Agent)
+admin.site.register(UserProfile)
