3cef41a9b94e5063f54f66b970fb72350985ef6e
Sylvester-Ad <sylvesteradade895@gmail.com>
2025-06-14 19:46:00 +0000

    Refactor Lead and Category models to ensure organization field is consistently defined

--- cs50w/python/django_projects/djcrm/leads/models.py
+++ cs50w/python/django_projects/djcrm/leads/models.py
@@ -19,10 +19,20 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+    organization = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey("Category", null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.age})"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30) #New, Contacted, Converted, Unconverted
+    organization = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
