e1288ab7e4aa94d695c20a720054041607ca26c8
Sylvester-Ad <sylvesteradade895@gmail.com>
2025-06-17 22:55:13 +0000

    Refactor UserProfile, Lead, Category, and Agent models to enhance related_name attributes for better query optimization and organization

--- cs50w/python/django_projects/djcrm/leads/models.py
+++ cs50w/python/django_projects/djcrm/leads/models.py
@@ -8,35 +8,40 @@ class User(AbstractUser):
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    organization_name = models.CharField(max_length=100, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user.username}"
 
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-    organization = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
-    category = models.ForeignKey("Category", null=True, blank=True, on_delete=models.SET_NULL)
+    organization = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="leads")
+    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL,  related_name="assigned_leads")
+    category = models.ForeignKey("Category", null=True, blank=True, on_delete=models.SET_NULL, related_name="leads")
+    description = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.age})"
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=30) #New, Contacted, Converted, Unconverted
-    organization = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    organization = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="categories")
 
     def __str__(self):
         return self.name
 
 class Agent(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    organization = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="agent_profile")
+    organization = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="agents")
 
     def __str__(self):
         return self.user.email
